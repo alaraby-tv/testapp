dade837019056c6a7ddfe8d42f10b45963dcfc39
alaraby-tv <pmarkowski@alaraby.tv>
2020-05-20 14:50:50 +0100

    uncomment out appear set deploy to

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:alaraby-tv/testapp.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/testapp"
+set :deploy_to, "/var/www/testapp"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
