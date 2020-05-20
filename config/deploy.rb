52d840629c93e30b9b1c4ac34da4bbac3242e51e
alaraby-tv <pmarkowski@alaraby.tv>
2020-05-20 13:27:53 +0100

    correct example to github

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.0"
 
 set :application, "testapp"
-set :repo_url, "git@example.com:alaraby-tv/testapp.git"
+set :repo_url, "git@github.com:alaraby-tv/testapp.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
